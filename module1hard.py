1f77866deba411eb641dd918d7ed41641a435d6a
RystikS <rustamsagadeev@gmail.com>
2024-07-29 01:26:16 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,10 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+abc = sorted(students)
+a = sum(grades[0])/len(grades[0])
+b = sum(grades[0])/len(grades[0])
+c = sum(grades[0])/len(grades[0])
+d = sum(grades[0])/len(grades[0])
+e = sum(grades[0])/len(grades[0])
+dict={abc[0]:a,abc[1]:b,abc[2]:c,abc[3]:d,abc[4]:e}
+print(dict)
