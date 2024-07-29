50481c259b7bd7b8d76edea3355874ffd688f522
RystikS <rustamsagadeev@gmail.com>
2024-07-29 07:38:44 +0300

    module1hard.py

--- module1hard.py
+++ module1hard.py
@@ -2,9 +2,9 @@ grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 abc = sorted(students)
 a = sum(grades[0])/len(grades[0])
-b = sum(grades[0])/len(grades[0])
-c = sum(grades[0])/len(grades[0])
-d = sum(grades[0])/len(grades[0])
-e = sum(grades[0])/len(grades[0])
+b = sum(grades[1])/len(grades[1])
+c = sum(grades[2])/len(grades[2])
+d = sum(grades[3])/len(grades[3])
+e = sum(grades[4])/len(grades[4])
 dict={abc[0]:a,abc[1]:b,abc[2]:c,abc[3]:d,abc[4]:e}
 print(dict)
